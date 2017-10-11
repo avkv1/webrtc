0112177aa2af90fdffe1a9c2c17e150eafb93095
Alex Kuleshov <772122@gmail.com>
2017-10-11 18:56:04 +0300

    Clean code

--- src/main/java/ru/akuleshov/webrtc/conf/WebSocketConfig.java
+++ src/main/java/ru/akuleshov/webrtc/conf/WebSocketConfig.java
@@ -1,10 +1,7 @@
 package ru.akuleshov.webrtc.conf;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import ru.akuleshov.webrtc.socket.SignalingSocketHandler;
@@ -17,16 +14,9 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-//        webSocketHandlerRegistry
-//                .addHandler(signalingSocketHandler(), "/signal")
-//                .setAllowedOrigins("*");
         webSocketHandlerRegistry
                 .addHandler(signalingSocketHandler, "/signal")
                 .setAllowedOrigins("*");
     }
 
-//    @Bean
-//    public WebSocketHandler signalingSocketHandler() {
-//        return new SignalingSocketHandler();
-//    }
 }
